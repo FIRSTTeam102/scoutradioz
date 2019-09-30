48a6b6c0e721a0c15ae8d76b4c6d85ecbcfa618b
JL102 <jordannamez@gmail.com>
2019-09-30 18:02:25 -0400

    working on updating teammembers for new user db

--- primary/routes/admin/teammembers.js
+++ primary/routes/admin/teammembers.js
@@ -1,48 +1,43 @@
 const router = require("express").Router();
 const utilities = require("../../utilities");
 
+//DONE
 router.get("/", async function(req, res) {
 	//Check authentication for team admin level
 	if( !await req.authenticate( process.env.ACCESS_TEAM_ADMIN ) ) return;
 	
-	var teamMembers = await utilities.find("teammembers", {}, {sort: {"name": 1}});
+	var org_key = req.user.org_key;
+	
+	var orgMembers = await utilities.find("users", {org_key: org_key, name: {$ne: "default_user"}}, {sort: {"name": 1}})
+	var org = await utilities.findOne("orgs", {org_key: org_key});
+	
+	var config = org.config.members;
+	/*This is the organization's configuration for member information.
+	  Currently, it contains:
+	  	{ "members": { 
+			"subteams": [
+				{ "label": "Mechanical", "subteam_key": "mech" },
+				{ "label": "Electrical", "subteam_key": "elec" }, 
+				{ "label": "Programming", "subteam_key": "prog" }, 
+				{ "label": "Drive", "subteam_key": "driv" }
+			], 
+			"classes": [
+				{ "label": "Freshman", "class_key": "freshman" },
+				{ "label": "Sophomore", "class_key": "sophomore" },
+				{ "label": "Junior", "class_key": "junior" }, 
+				{ "label": "Senior", "class_key": "senior" },
+				{ "label": "Alum", "class_key": "alum" }, 
+				{ "label": "Middle School", "class_key": "middleschool" }, 
+				{ "label": "Mentor", "class_key": "mentor" }, 
+				{ "label": "Parent", "class_key": "parent" }
+		]}} 
+	*/
 	
 	res.render("./admin/members", { 
-		title: "Team Members",
-		"members": teamMembers 
+		title: "Organization Members",
+		members: orgMembers,
+		config: config,
 	});
-});
-
-router.get("/present", async function(req, res) {
-	//Check authentication for team admin level
-	if( !await req.authenticate( process.env.ACCESS_TEAM_ADMIN ) ) return;
-	
-	var teamMembers = await utilities.find("teammembers", {}, {sort: {"name": 1}});
-		
-	res.render("./admin/present", {
-		title: "Assign Who Is Present",
-		"members": teamMembers
-	});
-});
-
-router.post("/updatepresent", async function(req, res){
-	//Check authentication for team admin level
-	if( !await req.authenticate( process.env.ACCESS_TEAM_ADMIN ) ) return;
-	
-	await utilities.update("teammembers", {}, { $set: { "present" : "false" } }, {multi: true});
-	
-	//Get a list of all present member IDs.
-	var allPresentMembers = [];
-	for(var i in req.body) allPresentMembers.push(i);
-	
-	res.log(`updatepresent: allPresentMembers: ${JSON.stringify(allPresentMembers)}`);
-	
-	var query = {"_id": {$in: allPresentMembers}};
-	var update = {$set: {"present": "true"}};
-	
-	await utilities.update("teammembers", query, update, {multi: true});
-	
-	res.redirect("./present");
 });
 
 router.post("/addmember", async function(req, res){
@@ -98,14 +93,12 @@ router.post("/updatemember", async function(req, res){
 	var thisFuncName = "teammembers.updatemember[post]: ";
 	res.log(thisFuncName + 'ENTER')
 	
+	var org_key = req.user.org_key;
 	var memberId = req.body.memberId;
 	var name = req.body.name;
-	var subteam = req.body.subteam;
-	var className = req.body.className;
+	var subteam_key = req.body.subteam_key;
+	var class_key = req.body.class_key;
 	var years = req.body.years;
-	
-	//log it
-	res.log(`Request to update member ${memberId} with details ${JSON.stringify(req.body)}`, true);
 	
 	// recalculate seniority
 	var seniority = years;
@@ -113,7 +106,7 @@ router.post("/updatemember", async function(req, res){
 	if (isNaN(parseInt(seniority))) seniority = "0";
 	
 	// Get the first 3 characters, all lower case
-	var classPre = className.toLowerCase().substring(0, 3);
+	var classPre = class_key.toLowerCase().substring(0, 3);
 	switch(classPre) {
 		case "fre":
 			seniority += ".1";
@@ -130,13 +123,29 @@ router.post("/updatemember", async function(req, res){
 		default:
 			seniority += ".0";
 	}
-	res.log(thisFuncName + 'seniority=' + seniority);
 	
-	res.log({memberId, name, subteam, className, years});
+	//Query to update user in db
+	var updateQuery = {$set: 
+		{name: name,
+		org_info: {
+			subteam_key: subteam_key,
+			class_key: class_key,
+			years: years,
+			seniority: seniority
+		}
+	}};
 	
-	await utilities.update("teammembers", {"_id": memberId}, {$set: {"name": name, "subteam": subteam, "className": className, "years": years, "seniority": seniority}});
+	//log it
+	res.log(`Request to update member ${memberId} with details ${JSON.stringify(updateQuery)}`, true);
 	
-	res.redirect("./");
+	var writeResult = await utilities.update("users", {org_key: org_key, _id: memberId}, updateQuery);
+	
+	console.log(writeResult);
+	
+	res.send({
+		status: 200,
+		message: `Updated ${name} successfully.`
+	});
 });
 
 router.post("/deletemember", async function(req, res){
@@ -157,6 +166,38 @@ router.post("/deletemember", async function(req, res){
 	}
 	
 	res.redirect('/admin/teammembers');
+});
+
+router.get("/present", async function(req, res) {
+	//Check authentication for team admin level
+	if( !await req.authenticate( process.env.ACCESS_TEAM_ADMIN ) ) return;
+	
+	var teamMembers = await utilities.find("teammembers", {}, {sort: {"name": 1}});
+	
+	res.render("./admin/present", {
+		title: "Assign Who Is Present",
+		"members": teamMembers
+	});
+});
+
+router.post("/updatepresent", async function(req, res){
+	//Check authentication for team admin level
+	if( !await req.authenticate( process.env.ACCESS_TEAM_ADMIN ) ) return;
+	
+	await utilities.update("teammembers", {}, { $set: { "present" : "false" } }, {multi: true});
+	
+	//Get a list of all present member IDs.
+	var allPresentMembers = [];
+	for(var i in req.body) allPresentMembers.push(i);
+	
+	res.log(`updatepresent: allPresentMembers: ${JSON.stringify(allPresentMembers)}`);
+	
+	var query = {"_id": {$in: allPresentMembers}};
+	var update = {$set: {"present": "true"}};
+	
+	await utilities.update("teammembers", query, update, {multi: true});
+	
+	res.redirect("./present");
 });
 
 module.exports = router;
