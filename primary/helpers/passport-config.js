bbda7669616a6fbe6a6906125f99a39fa2758f92
JL102 <jordannamez@gmail.com>
2019-10-30 15:03:53 -0400

    added trace logging on passport funcs

--- primary/helpers/passport-config.js
+++ primary/helpers/passport-config.js
@@ -1,16 +1,19 @@
 const passport = require('passport');
+const logger = require('log4js').getLogger();
 //const LocalStrategy = require('passport-local').Strategy; //strategy for passport
 const utilities = require("../utilities");
 
 // Creates the data necessary to store in the session cookie
 passport.serializeUser(function(user, done) {
 	//if we switch to mongoose, change to done(null, user.id);
-	console.log("serializeUser:"+user._id);
+	logger.trace("serializeUser: " + user._id);
     done(null, user._id);
 });
 
 // Reads the session cookie to determine the user from a user ID
 passport.deserializeUser(async function(id, done) {
+	
+	logger.trace('deserializeUser: ' + id);
 	
 	var user = await utilities.findOne("users", { "_id": id }, {});
 	
