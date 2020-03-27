962e2e61917d73003f9f30614e69cf8538c42622
JL102 <jordannamez@gmail.com>
2020-03-27 15:24:03 -0400

    alias versioning on webhook

--- webhook/webhook.js
+++ webhook/webhook.js
@@ -7,19 +7,36 @@ const webpush = require('web-push');
 const utilities = require('@firstteam102/scoutradioz-utilities');
 const matchDataHelper = require('@firstteam102/scoutradioz-helpers').matchData;
 
-//Configure utilities with the full file path of our databases json file
+//utililties config
 utilities.config(require('./databases.json'));
+
+//log4js config
 log4js.configure({
-	appenders: { out: { type: 'stdout', layout: { type: 'basic' } } },
+	appenders: { out: { type: 'stdout', layout: {
+		type: 'pattern',
+		//Non-colored pattern layout (default)
+		pattern: '[%x{tier}] [%p] %c.%x{funcName} - %m',
+		tokens: {
+			'tier': logEvent => process.env.ALIAS,
+			'funcName': logEvent => {
+				if (logEvent.context && logEvent.context.funcName) {
+					return logEvent.context.funcName;
+				}
+				else return '';
+			},
+		},
+	} } },
 	categories: { default: { appenders: ['out'], level: 'info' } }
 });
-const logger = log4js.getLogger();
-logger.level = 'debug';
+const logger = log4js.getLogger('webhook');
+logger.level = 'trace';
 
-//Create Express app for webhooks
+//EXPRESS APP SETUP
 const webhook = express();
-const router = express.Router();
+module.exports = webhook;
 
+//utilities tier refresh
+webhook.use(utilities.refreshTier);
 //bodyParser config
 const options = {
 	extended: false,
@@ -44,27 +61,75 @@ const options = {
 webhook.use(bodyParser.json(options));
 webhook.use(bodyParser.urlencoded(options));
 
+//ROUTER
+const router = express.Router();
+//URL splitter
+webhook.use((req, res, next) => {
+	logger.addContext('funcName', 'urlParser');
+	
+	//Tier is overridden in lambda.js.
+	var tier = process.env.TIER;
+	logger.info(`tier=${tier} originalUrl=${req.originalUrl} url=${req.url}`);
+	
+	var splitUrl = req.url.split('/');
+	logger.debug(splitUrl);
+	
+	if (splitUrl[1] == tier) {
+		logger.info('URL includes tier. Cutting it out of url.');
+		//Remove tier from url
+		splitUrl.splice(1, 1);
+		req.url = splitUrl.join('/');
+		//if url does not start with a slash, add a slash to avoid breaking something
+		if (req.url[0] != '/') req.url = '/' + req.url;
+		logger.info(`new req.url=${req.url}`);
+		next();
+	}
+	//If current tier is not in the url, send a 404
+	else {
+		logger.info('URL does not include tier. Sending 404.');
+		res.status(404).send('Not found. Must include process tier in URL.');
+	}
+});
 webhook.use('/', router);
+//404 handler
+webhook.use((req, res, next) => {
+	var err = new Error('Not Found');
+	err.status = 404;
+	next(err);
+});
+//Error handler
+webhook.use((err, req, res, next) => {
+	logger.addContext('funcName', 'error');
+	
+	logger.error(err.message || err, JSON.stringify(err.stack));
+	
+	res.status(err.status || 500).send(err);
+});
+
+router.all('/*', wrap(async (req, res, next) => {
+	logger.removeContext('funcName');
+	next();
+}))
 
 //Routing
 router.get('/', wrap(async (req, res) => {
+	logger.addContext('funcName', 'root[get]');
 	
 	logger.info(JSON.stringify(req.query));
 	
 	res.send(req.query);
-	
 }));
 
 router.post('/', wrap(async (req, res) => {
-	var thisFuncName = "webhook.[root/post]: ";
+	logger.addContext('funcName', 'root[post]');
 	
 	var message = req.body;
-	logger.info(thisFuncName + "ENTER message=" + JSON.stringify(message));
+	logger.info("ENTER message=" + JSON.stringify(message));
 
     var messageType = message.message_type;
 	var messageData = message.message_data;
 	
-    logger.info(thisFuncName + "messageType=" + messageType);
+    logger.info("messageType=" + messageType);
 	
 	//Delegate data handling to separate functions.
 	switch(messageType){
@@ -90,9 +155,10 @@ router.post('/', wrap(async (req, res) => {
 			await handleAwardsPosted( messageData );
 			break;
 		default:
-			logger.info(thisFuncName + "unknown messageType-" + messageType + ",messageData=" + JSON.stringify(messageData));
+			logger.info("unknown messageType-" + messageType + ",messageData=" + JSON.stringify(messageData));
 	}
 	
+	logger.info('DONE');
 	res.status(200).send("thanks!");
 }));
 
@@ -100,12 +166,12 @@ router.post('/', wrap(async (req, res) => {
 
 //TBA push handlers
 async function handleUpcomingMatch( data ) {
-	var thisFuncName = 'webhook.handleUpcomingMatch(): ';
+	logger.addContext('funcName', 'handleUpcomingMatch');
 
 	var match_key = data.match_key;
 	var event_key = match_key.split('_')[0];
 	var event_year = parseInt(event_key.substring(0, 4));
-	logger.info(thisFuncName + "ENTER event_year=" + event_year + ",event_key=" + event_key + ",match_key=" + match_key);
+	logger.info("ENTER event_year=" + event_year + ",event_key=" + event_key + ",match_key=" + match_key);
 
 	// Synchronize the rankings (just in case)
 	await syncRankings(event_key);
@@ -113,13 +179,13 @@ async function handleUpcomingMatch( data ) {
 	// push notifications
 	if (process.env.disablePushNotifications != 'true') {
 	
-		logger.debug(`${thisFuncName} Configuring web-push`);
+		logger.debug(`Configuring web-push`);
 		const keys = await utilities.findOne('passwords', {name: 'web_push_keys'});
 		webpush.setVapidDetails('mailto:roboticsfundinc@gmail.com', keys.public_key, keys.private_key);
 		
 		const teamKeys = data.team_keys;
 		const matchNumberKey = data.match_key.split('_')[1];
-		logger.debug(`${thisFuncName} matchNumberKey: ${matchNumberKey}, teamKeys: ${JSON.stringify(teamKeys)}`);
+		logger.debug(`matchNumberKey: ${matchNumberKey}, teamKeys: ${JSON.stringify(teamKeys)}`);
 		
 		for (var teamKey of teamKeys) {
 			//find assignee of this team key
@@ -127,7 +193,7 @@ async function handleUpcomingMatch( data ) {
 			const assigneeID = '5d9cd6951c4f783330139549';
 			const user = await utilities.findOne('users', {_id: assigneeID});
 			
-			logger.info(`${thisFuncName} Asignee: ${JSON.stringify(user)}`);
+			logger.info(`Asignee: ${JSON.stringify(user)}`);
 			
 			//if user is found in the db, proceed
 			if (user) {
@@ -163,7 +229,7 @@ async function handleUpcomingMatch( data ) {
 						titleIdentifier = `Final Match ${matchNumber}`;
 					} 
 					else {
-						throw new Error(`${thisFuncName} Unexpected matchKey comp_level identifier`);
+						throw new Error(`Unexpected matchKey comp_level identifier`);
 					}
 					
 					var imageHref = 'https://upload.scoutradioz.com/app/generate/upcomingmatch?'
@@ -200,7 +266,7 @@ async function handleUpcomingMatch( data ) {
 					await sendPushMessage(pushSubscription, notificationContent);
 				}
 				else {
-					logger.debug(`${thisFuncName} Push subscription not available for ${req.user.name}`);
+					logger.debug(`Push subscription not available for ${req.user.name}`);
 			
 				}
 			}
@@ -225,13 +291,12 @@ async function handleUpcomingMatch( data ) {
 }
 
 async function handleMatchScore( data ) {
-	var thisFuncName = "webhook.handleMatchScore(): ";
-	//logger.debug(thisFuncName + "ENTER");
+	logger.addContext('funcName', 'handleMatchScore');
 	
 	var match_key = data.match.key;
 	var event_key = match_key.split('_')[0];
 	var event_year = parseInt(event_key.substring(0, 4));
-	logger.info(thisFuncName + "ENTER event_year=" + event_year + ",event_key=" + event_key + ",match_key=" + match_key);
+	logger.info("ENTER event_year=" + event_year + ",event_key=" + event_key + ",match_key=" + match_key);
 
 	// 2020-02-13, M.O'C: Handle possible bugs in webhook push data?
 	// Setting winning_alliance
@@ -284,8 +349,8 @@ async function handleMatchScore( data ) {
 }
 
 async function handleStartingCompLevel( data ) {
-	var thisFuncName = "webhook.handleStartingCompLevel(): ";
-	logger.info(thisFuncName + "ENTER (sync rankings only) data=" + JSON.stringify(data));
+	logger.addContext('funcName', 'handleStartingCompLevel');
+	logger.info("ENTER (sync rankings only) data=" + JSON.stringify(data));
 	var event_key = data.event_key; // <-- Comment this out & send 'starting_comp_level' webhooks from TBA to cause errors
 	
 	// Synchronize the rankings
@@ -293,30 +358,27 @@ async function handleStartingCompLevel( data ) {
 }
 
 async function handleAllianceSelection( data ) {
-	var thisFuncName = "webhook.handleAllianceSelection(): ";
-	logger.info(thisFuncName + "ENTER DNGN data=" + JSON.stringify(data));
-	
+	logger.addContext('funcName', 'handleAllianceSelection');
+	logger.info("ENTER DNGN data=" + JSON.stringify(data));
 }
 
 async function handleScheduleUpdated( data ) {
-	var thisFuncName = "webhook.handleScheduleUpdated(): ";
-	//logger.debug(thisFuncName + "data=" + JSON.stringify(data));
+	logger.addContext('funcName', 'handleScheduleUpdated');
 	/*
-{
-    "event_name": "FIM District East Kentwood Event",
-    "first_match_time": 1553871600,
-    "event_key": "2019miken"
-}	
+	{
+		"event_name": "FIM District East Kentwood Event",
+		"first_match_time": 1553871600,
+		"event_key": "2019miken"
+	}	
 	*/
-	//return;
 
 	var event_key = data.event_key;
 	var event_year = parseInt(event_key.substring(0, 4));
-	logger.info(thisFuncName + "ENTER event_year=" + event_year + ",event_key=" + event_key);
+	logger.info("ENTER event_year=" + event_year + ",event_key=" + event_key);
 
 	// Reload the matches
 	var url = "event/" + event_key + "/matches";
-	logger.debug(thisFuncName + "url=" + url);
+	logger.debug("url=" + url);
 	var matchData = await utilities.requestTheBlueAlliance(url);
 	var array = JSON.parse(matchData);
 	if (array && array.length && array.length > 0) {
@@ -333,21 +395,20 @@ async function handleScheduleUpdated( data ) {
 }
 
 async function handleAwardsPosted( data ) {
-	var thisFuncName = "webhook.handleAwardsPosted(): ";
-	logger.info(thisFuncName + "ENTER DNGN data=" + JSON.stringify(data));
-	
+	logger.addContext('funcName', 'handleAwardsPosted');
+	logger.info("ENTER DNGN data=" + JSON.stringify(data));
 }
 
 ////////// Helper functions
 
 // Pull down rankings for event event_key
 async function syncRankings(event_key) {
-	var thisFuncName = "webhook.syncRankings(): ";
-	logger.info(thisFuncName + "ENTER");
+	logger.addContext('funcName', 'syncRankings');
+	logger.info("ENTER");
 
 	// Reload the rankings from TBA
 	var rankingUrl = "event/" + event_key + "/rankings";
-	logger.info(thisFuncName + "rankingUrl=" + rankingUrl);
+	logger.info("rankingUrl=" + rankingUrl);
 
 	var rankData = await utilities.requestTheBlueAlliance(rankingUrl);
 	var rankinfo = JSON.parse(rankData);
@@ -362,7 +423,7 @@ async function syncRankings(event_key) {
 			rankArr.push(thisRank);
 		}
 	}
-	logger.trace(thisFuncName + 'rankArr=' + JSON.stringify(rankArr));
+	logger.trace('rankArr=' + JSON.stringify(rankArr));
 
 	// 2020-02-08, M.O'C: Change 'currentrankings' into event-specific 'rankings' 
 	// Delete the current rankings
@@ -375,8 +436,8 @@ async function syncRankings(event_key) {
 
 // Push notification function
 async function sendPushMessage(subscription, dataToSend) {
-	var thisFuncName = "webhook.sendPushMessage(): ";
-	logger.info(thisFuncName + "ENTER");
+	logger.addContext('funcName', 'sendPushMessage');
+	logger.info("ENTER");
 	
 	logger.debug(`Attempting to send push message: ${dataToSend}`);
 	
@@ -394,5 +455,3 @@ async function sendPushMessage(subscription, dataToSend) {
 		logger.error(err);
 	}
 }
-
-module.exports = webhook;
