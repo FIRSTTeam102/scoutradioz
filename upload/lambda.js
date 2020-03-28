5ff47646cf10a7ed898cdb4839a5c844af08c74f
JL102 <jordannamez@gmail.com>
2020-03-27 20:51:28 -0400

    reformatted lambdajs

--- upload/lambda.js
+++ upload/lambda.js
@@ -1,39 +1,40 @@
-'use strict'
-const awsServerlessExpress = require(process.env.NODE_ENV === 'test' ? '../../index' : 'aws-serverless-express')
-const app = require('./app')
+'use strict';
+const awsServerlessExpress = require(process.env.NODE_ENV === 'test' ? '../../index' : 'aws-serverless-express');
+const app = require('./app');
 
 // NOTE: If you get ERR_CONTENT_DECODING_FAILED in your browser, this is likely
 // due to a compressed response (e.g. gzip) which has not been handled correctly
 // by aws-serverless-express and/or API Gateway. Add the necessary MIME types to
 // binaryMimeTypes below, then redeploy (`npm run package-deploy`)
 const binaryMimeTypes = [
-  'application/javascript',
-  'application/json',
-  'application/octet-stream',
-  'application/xml',
-  'font/eot',
-  'font/opentype',
-  'font/otf',
-  'image/jpeg',
-  'image/png',
-  'image/svg+xml',
-  'text/comma-separated-values',
-  'text/css',
-  'text/html',
-  'text/javascript',
-  'text/plain',
-  'text/text',
-  'text/xml'
-]
-const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes)
+	'application/javascript',
+	'application/json',
+	'application/octet-stream',
+	'application/xml',
+	'font/eot',
+	'font/opentype',
+	'font/otf',
+	'image/jpeg',
+	'image/png',
+	'image/svg+xml',
+	'text/comma-separated-values',
+	'text/css',
+	'text/html',
+	'text/javascript',
+	'text/plain',
+	'text/text',
+	'text/xml'
+];
+const server = awsServerlessExpress.createServer(app, null, binaryMimeTypes);
 
 exports.handler = (event, context) => {
   
-  var alias = context.invokedFunctionArn.replace(/.*:/g,'');
-  console.log("ALIAS: "+ alias);
+	var alias = context.invokedFunctionArn.replace(/.*:/g,'');
+	console.log('ALIAS: '+ alias);
   
-  process.env.ALIAS = alias;
-  process.env.TIER = alias.toLowerCase();
+	process.env.ALIAS = alias;
+	//process.env.TIER is overridden here during every request.
+	process.env.TIER = alias.toLowerCase();
 
-  return awsServerlessExpress.proxy(server, event, context);
-} 
+	return awsServerlessExpress.proxy(server, event, context);
+}; 
