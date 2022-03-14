9eeadc9855ebc4867f90108f0391ce2b7690a9cf
JL102 <jordanlees@mailbox.org>
2022-03-13 21:06:02 -0400

    Added (optional) sticky bar on match scouting to improve the experience on phones

--- primary/public/js/script-matchscouting.js
+++ primary/public/js/script-matchscouting.js
@@ -1,5 +1,169 @@
-/* eslint-disable no-undef */
-/* eslint-disable no-unused-vars */
+$(function () {
+	
+	$('#toggleStickyToggle').addClass('animate');
+	
+	$('#toggleSticky').on('change', () => {
+		var enabled = $('#toggleSticky').prop('checked');
+		window.stickyBarEnabled = enabled;
+		if (enabled) {
+			// Show the sticky bar
+			$('#stickyBar').show().css('bottom', -60).animate({
+				'bottom': 0
+			}, 200);
+			// Save the fact that it's enabled
+			localStorage.removeItem('disableStickyBar');
+		}
+		else {
+			// Hide the sticky bar
+			$('#stickyBar').animate({
+				'bottom': -60
+			}, 200);
+			// Clear the "disabled-form" class from all elements
+			$('.disabled-form').removeClass('disabled-form');
+			// Save the fact that it's disabled
+			localStorage.setItem('disableStickyBar', '1');
+		}
+	});
+	
+	//- Get a list of all the header elements, to use for scrolling
+	var headerElements = [];
+	for (let label of window.headerList) {
+		let thisHeader = $(`#${label}`);
+		if (thisHeader[0]) headerElements.push(thisHeader[0]);
+	}
+				
+	const stickyBarTitle = $('#stickyBarTitle');
+	const stickyBarLeft  = $('#stickyBarLeft');
+	const stickyBarRight = $('#stickyBarRight');
+				
+	handleScroll();
+				
+	var ticking = false;
+	$(window).on('scroll', () => {
+		if (!ticking) {
+			requestAnimationFrame(() => {
+				handleScroll();
+				ticking = false;
+			});
+		}
+		ticking = true;
+	});
+				
+	var currentHeaderID = '';
+				
+	function handleScroll() {
+		if (!window.stickyBarEnabled) return;
+		
+		for (let header of headerElements) {
+			let isVisible = checkVisible(header);
+			if (isVisible) {
+				//- Only update the sticky bar when it needs to change
+				if (currentHeaderID !== header.id) {
+					setStickyBar(header.id, header.innerText);
+				}
+				break;
+			}
+		}
+	}
+				
+	function setStickyBar(id, text) {
+		let st = performance.now();
+		stickyBarTitle.text(text);
+		currentHeaderID = id;
+					
+		//- Now, update the visibility of the elements under the OTHER headers
+		for (let i = 0; i < headerElements.length; i++) {
+			let header = headerElements[i];
+			let nextHeader = headerElements[i + 1];
+			let thisSibling = header.nextElementSibling;
+						
+			if (header.id === id) {
+				header.classList.remove('disabled-form');
+				while(thisSibling && thisSibling !== nextHeader) {
+					thisSibling.classList.remove('disabled-form');
+					thisSibling = thisSibling.nextElementSibling;
+				}
+			}
+			else {
+				header.classList.add('disabled-form');
+				while(thisSibling && thisSibling !== nextHeader) {
+					thisSibling.classList.add('disabled-form');
+					thisSibling = thisSibling.nextElementSibling;
+				}
+			}
+		}
+		console.log(performance.now() - st);
+	}
+				
+	stickyBarLeft.on('click', () => {
+		let currentHeader = document.getElementById(currentHeaderID);
+		let index = headerElements.indexOf(currentHeader);
+		let prevHeader = headerElements[index - 1];
+		if (prevHeader) {
+			window.scrollToId(prevHeader.id);
+		}
+	});
+				
+	stickyBarRight.on('click', () => {
+		let currentHeader = document.getElementById(currentHeaderID);
+		let index = headerElements.indexOf(currentHeader);
+		let nextHeader = headerElements[index + 1];
+		if (nextHeader) {
+			window.scrollToId(nextHeader.id);
+		}
+	});
+				
+	//- Just some visual flourish
+	stickyBarLeft.on('mousedown', fancyMouseDown);
+	stickyBarLeft.on('touchstart', fancyMouseDown);
+	stickyBarRight.on('mousedown', fancyMouseDown);
+	stickyBarRight.on('touchstart', fancyMouseDown);
+	stickyBarLeft.on('mouseup', fancyMouseUp);
+	stickyBarLeft.on('touchend', fancyMouseUp);
+	stickyBarRight.on('mouseup', fancyMouseUp);
+	stickyBarRight.on('touchend', fancyMouseUp);
+				
+	function fancyMouseDown() {
+		$(this).css({
+			top: 2,
+			left: 2,
+		});
+	}
+	function fancyMouseUp() {
+		$(this).css({
+			top: '',
+			left: '',
+		});
+	}
+				
+	//- Credit: https://stackoverflow.com/questions/5353934/check-if-element-is-visible-on-screen
+	function checkVisible(elm) {
+		var rect = elm.getBoundingClientRect();
+		var viewHeight = Math.max(document.documentElement.clientHeight, window.innerHeight);
+		return !(rect.bottom < 0 || rect.top - viewHeight >= 0);
+	}
+				
+	window.onResize(() => {
+		if (!window.stickyBarEnabled) return;
+		
+		// - The text is centered at a fixed size, but we want to make sure the text is visible even on small screens.
+		let triLeft = $('#stickyBarLeft')[0];
+		let text = $('#stickyBarTitle')[0];
+		const OFFSET = -8; // the buttons have a lot of padding; allowing a bit of overlap
+					
+		text.style.fontSize = '1em';
+		let triRect = triLeft.getBoundingClientRect();
+		//- This just loops from 1 to 10 and iteratively reduces the font size until it fits without overlapping the triangle icons.
+		let iterations = 0, textSize = 1;
+		while (iterations < 10 && text.getBoundingClientRect().left < triRect.right + OFFSET) {
+			textSize -= 0.05;
+			text.style.fontSize = textSize + 'em';
+			iterations++;
+		}
+	});
+
+});
+
 $(function(){
 	
 	$('#submit').on('click', function(){
@@ -26,5 +190,5 @@ $(function(){
 			return 'Leaving this page will lose match scouting data.';
 		};
 	});
-
+	
 });
