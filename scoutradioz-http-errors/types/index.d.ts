5135ae44b93c7cfca9e8ad3b0ecd482ebefebc1e
JL102 <jordanlees@mailbox.org>
2022-05-06 00:14:55 -0400

    http-errors 1.0.4 - typescript

---
+++ scoutradioz-http-errors/types/index.d.ts
@@ -0,0 +1,28 @@
+export declare class HttpError extends Error {
+    status: number;
+    constructor(message?: string);
+}
+export declare class UserError extends HttpError {
+    constructor(message?: string);
+}
+export declare class UnauthorizedError extends HttpError {
+    constructor(message?: string);
+}
+export declare class ForbiddenError extends HttpError {
+    constructor(message?: string);
+}
+export declare class NotFoundError extends HttpError {
+    constructor(message?: string);
+}
+export declare class TeapotError extends HttpError {
+    constructor(message?: string);
+}
+export declare class TooEarlyError extends HttpError {
+    constructor(message?: string);
+}
+export declare class InternalServerError extends HttpError {
+    constructor(message?: string);
+}
+export declare class ServiceUnavailableError extends HttpError {
+    constructor(message?: string);
+}
