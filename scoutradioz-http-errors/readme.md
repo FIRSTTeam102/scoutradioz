8009301f46f8c0cf4f92d1db9e8b4420712d3aa4
JL102 <jordanlees@mailbox.org>
2022-05-06 00:14:55 -0400

    http-errors 1.0.4 - typescript

--- scoutradioz-http-errors/readme.md
+++ scoutradioz-http-errors/readme.md
@@ -1,3 +1,5 @@
+## http-errors
+
 A very basic module that defines errors with HTTP status codes, such as 400 Bad Request.
 https://scoutradioz.com
 
@@ -10,3 +12,8 @@ router.get('/throwanerror', function(req, res) {
 	throw new e.UserError('Invalid input');
 });
 ```
+
+To update:
+1. `tsc`
+1. Edit `package.json` with a new version number
+1. `npm publish`
--- scoutradioz-http-errors/src/index.ts
+++ scoutradioz-http-errors/src/index.ts
@@ -1,54 +1,62 @@
-class UserError extends Error {
-	constructor(message) {
+export class HttpError extends Error {
+	status: number;
+	constructor(message?: string) {
+		super(message);
+		this.status = -1;
+	}
+}
+
+export class UserError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Bad Request');
 		this.status = 400;
 	}
 }
 
-class UnauthorizedError extends Error {
-	constructor(message) {
+export class UnauthorizedError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Unauthorized');
 		this.status = 401;
 	}
 }
 
-class ForbiddenError extends Error {
-	constructor(message) {
+export class ForbiddenError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Forbidden');
 		this.status = 403;
 	}
 }
 
-class NotFoundError extends Error {
-	constructor(message) {
+export class NotFoundError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Not Found');
 		this.status = 404;
 	}
 }
 
-class TeapotError extends Error {
-	constructor(message) {
+export class TeapotError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'I\'m a teapot');
 		this.status = 418;
 	}
 }
 
-class TooEarlyError extends Error {
-	constructor(message) {
+export class TooEarlyError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Too Early');
 		this.status = 425;
 	}
 }
 
-class InternalServerError extends Error {
-	constructor(message) {
+export class InternalServerError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Internal Server Error');
 		this.status = 500;
 	}
 }
 
-class ServiceUnavailableError extends Error {
-	constructor(message) {
+export class ServiceUnavailableError extends HttpError {
+	constructor(message?: string) {
 		super(message || 'Service Unavailable');
 		this.status = 503;
 	}
