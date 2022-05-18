18dd37046a841c632937ba22fda6337f007770a4
JL102 <jordanlees@mailbox.org>
2022-05-17 20:09:16 -0400

    #63 Fixed some issues with the helpers package

--- scoutradioz-helpers/src/index.ts
+++ scoutradioz-helpers/src/index.ts
@@ -1,22 +1,27 @@
 'use strict';
-
-const upload = require('./uploadhelper');
-const matchData = require('./matchdatahelper');
-
 import { Utilities } from '@firstteam102/scoutradioz-utilities';
 
+import upload from './uploadhelper';
+import matchData from './matchdatahelper';
+
+// Helpers class, to configure in app.js
 export class Helpers {
-	upload = upload;
-	matchData = matchData;
+	static upload = upload;
+	static matchData = matchData;
 	
 	/**
 	 * Required: 
 	 * @param {object} utilities Already-configured @firstteam102/scoutradioz-utilities module
 	 */
-	config = (utilities: Utilities) => {
+	static config = (utilities: Utilities) => {
 		this.upload.config(utilities);
 		this.matchData.config(utilities);
 	};
 }
 
-module.exports = new Helpers();
+module.exports = Helpers;
+export default Helpers;
+
+// To allow individual imports
+export { default as upload } from './uploadhelper';
+export { default as matchData } from './matchdatahelper';
