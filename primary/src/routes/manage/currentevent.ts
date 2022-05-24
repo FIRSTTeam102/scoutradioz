4b3c2c50eeede478a2a2cac47dcb91614982a190
JL102 <jordanlees@mailbox.org>
2022-05-24 16:46:17 -0400

    #63 TS for manage/index and manage/currentevent

--- primary/src/routes/manage/currentevent.ts
+++ primary/src/routes/manage/currentevent.ts
@@ -1,13 +1,18 @@
-const router = require('express').Router();
-const logger = require('log4js').getLogger('currentevent');
-const wrap = require('express-async-handler');
-const utilities = require('@firstteam102/scoutradioz-utilities');
-const matchDataHelper = require('@firstteam102/scoutradioz-helpers').matchData;
+import express from 'express';
+import { getLogger } from 'log4js';
+import wrap from '../../helpers/express-async-handler';
+import utilities from '@firstteam102/scoutradioz-utilities';
+import Permissions from '../../helpers/permissions';
+import { Role, Org, User, Layout, Match, Ranking } from '@firstteam102/scoutradioz-types';
+import { matchData as matchDataHelper } from '@firstteam102/scoutradioz-helpers';
+
+const router = express.Router();
+const logger = getLogger('currentevent');
 
 router.all('/*', wrap(async (req, res, next) => {
 	logger.removeContext('funcName');
 	//Require team-admin-level authentication for every method in this route.
-	if (await req.authenticate (process.env.ACCESS_TEAM_ADMIN)) {
+	if (await req.authenticate (Permissions.ACCESS_TEAM_ADMIN)) {
 		next();
 	}
 }));
@@ -16,10 +21,10 @@ router.get('/matches', wrap(async (req, res) => {
 	logger.addContext('funcName', 'matches[get]');
 	logger.info('ENTER');
 	
-	var eventKey = req.event.key;
+	let eventKey = req.event.key;
 		
 	// Read matches from DB for specified event
-	var matches = await utilities.find('matches', {'event_key': eventKey},{sort: {'time': 1}});
+	let matches: Match[] = await utilities.find('matches', {'event_key': eventKey},{sort: {'time': 1}});
 		
 	res.render('./manage/currentevent/matches', {
 		title: 'Matches',
@@ -32,15 +37,15 @@ router.get('/getcurrentteams', wrap(async (req, res) => {
 	logger.info('ENTER');
 
 	// 2020-02-09, M.O'C: Refactoring to just update the team_keys for the current event
-	var event_key = req.event.key;
+	let event_key = req.event.key;
 
 	// Get the current event
 	//var thisEventData = await utilities.find("events", {"key": event_key});
 	//var thisEvent = thisEventData[0];
 
 	// Refresh the teams list from TBA
-	var eventTeamsUrl = `event/${event_key}/teams/keys`;
-	var thisTeamKeys = await utilities.requestTheBlueAlliance(eventTeamsUrl);
+	let eventTeamsUrl = `event/${event_key}/teams/keys`;
+	let thisTeamKeys = await utilities.requestTheBlueAlliance(eventTeamsUrl);
 	await utilities.update( 'events', {'key': event_key}, {$set: {'team_keys': thisTeamKeys}} );
 
 	res.redirect('/manage?alert=Updated team keys for the current event successfully.');
@@ -56,10 +61,10 @@ router.post('/resetmatches', wrap(async (req, res) => {
 	// 	return res.redirect('/manage/currentevent/matches?alert=Unauthorized to modify TBA data.');
 	// }
 	
-	var eventKey = req.event.key;
+	let eventKey = req.event.key;
 	
 	// update all matches - set 'actualtime' to null/"", and team scores to -1
-	var writeResult = await utilities.bulkWrite('matches', [{updateMany:{
+	let writeResult = await utilities.bulkWrite('matches', [{updateMany:{
 		filter: {event_key: eventKey}, 
 		update: {$set: {actual_time : '', winning_alliance : '', 'alliances.blue.score': -1, 'alliances.red.score': -1 }}}
 	}]);
@@ -71,12 +76,12 @@ router.post('/resetmatches', wrap(async (req, res) => {
 router.post('/resetmatch', wrap(async (req, res) => {
 	logger.addContext('funcName', 'resetmatch[post]');
 	
-	var matchKey = req.body.matchId;
+	let matchKey = req.body.matchId;
 	
 	if (!matchKey) return res.redirect('/manage/currentevent/matches?alert=Match not specified.&type=warn');
 	logger.info(`Resetting match ${matchKey}`);
 	
-	var writeResult = await utilities.update('matches', 
+	let writeResult = await utilities.update('matches', 
 		{key: matchKey}, 
 		{$set: { actual_time: '', winning_alliance: '', 'alliances.blue.score': -1, 'alliances.red.score': -1 }}
 	);
@@ -94,36 +99,36 @@ router.post('/updatematch', wrap(async (req, res) => {
 	// 	return res.redirect('/manage/currentevent/matches?alert=Unauthorized to modify TBA data.');
 	// }
 	
-	var matchId = req.body.matchId;
+	let matchId = req.body.matchId;
 
-	var event_year = req.event.year;
-	var event_key = req.event.key;
-	var org_key = req.user.org_key;
+	let event_year = req.event.year;
+	let event_key = req.event.key;
+	let org_key = req._user.org_key;
 		
-	var eventKey = req.event.key;
+	let eventKey = req.event.key;
 	
 	// While we're here - Get the latest ranking (& OPR data...? maybe not?)
 	// https://www.thebluealliance.com/api/v3/event/2018njfla/rankings
 	// https://www.thebluealliance.com/api/v3/event/2018njfla/oprs (?)
 
 	// Reload the rankings from TBA
-	var rankingUrl = 'event/' + eventKey + '/rankings';
+	let rankingUrl = 'event/' + eventKey + '/rankings';
 	logger.debug('rankingUrl=' + rankingUrl);
 
-	var rankinfo = await utilities.requestTheBlueAlliance(rankingUrl);
-	var rankArr = [];
+	let rankinfo = await utilities.requestTheBlueAlliance(rankingUrl);
+	let rankArr: Ranking[] = [];
 	if (rankinfo && rankinfo.rankings && rankinfo.rankings.length > 0) {
 		// 2020-02-08, M.O'C: Change 'currentrankings' into event-specific 'rankings'; enrich with event_key 
-		var thisRankings = rankinfo.rankings;
-		for (var thisRank of thisRankings) {
+		let thisRankings = rankinfo.rankings;
+		for (let thisRank of thisRankings) {
 			thisRank['event_key'] = eventKey;
 			rankArr.push(thisRank);
 		}
 	}
 	//logger.debug('rankArr=' + JSON.stringify(rankArr));
 
-	var rankMap = {};
-	for (var rankIdx = 0; rankIdx < rankArr.length; rankIdx++) {
+	let rankMap: Dict<Ranking> = {};
+	for (let rankIdx = 0; rankIdx < rankArr.length; rankIdx++) {
 		//logger.debug('rankIdx=' + rankIdx + ', team_key=' + rankings[rankIdx].team_key + ', rank=' + rankings[rankIdx].rank);
 		rankMap[rankArr[rankIdx].team_key] = rankArr[rankIdx];
 	}
@@ -140,9 +145,9 @@ router.post('/updatematch', wrap(async (req, res) => {
 	await utilities.remove('matches', {'key': matchId});
 
 	// Reload the match data from TBA
-	var url = 'match/' + matchId;
+	let url = 'match/' + matchId;
 	logger.debug('url=' + url);
-	var match = await utilities.requestTheBlueAlliance(url);
+	let match = await utilities.requestTheBlueAlliance(url);
 	
 	// Now, insert the new object
 	await utilities.insert('matches', match);
@@ -172,24 +177,24 @@ router.post('/updatematches', wrap(async (req, res) => {
 	// }
 	
 	//var matchId = req.body.matchId;
-	var eventKey = req.event.key;
-	var event_year = req.event.year;
-	var org_key = req.user.org_key;
+	let eventKey = req.event.key;
+	let event_year = req.event.year;
+	let org_key = req._user.org_key;
 
 	// While we're here - Get the latest ranking (& OPR data...? maybe not?)
 	// https://www.thebluealliance.com/api/v3/event/2018njfla/rankings
 	// https://www.thebluealliance.com/api/v3/event/2018njfla/oprs (?)
 
 	// Reload the rankings from TBA
-	var rankingUrl = 'event/' + eventKey + '/rankings';
+	let rankingUrl = 'event/' + eventKey + '/rankings';
 	logger.debug('rankingUrl=' + rankingUrl);
 
-	var rankinfo = await utilities.requestTheBlueAlliance(rankingUrl);
-	var rankArr = [];
+	let rankinfo = await utilities.requestTheBlueAlliance(rankingUrl);
+	let rankArr = [];
 	if (rankinfo && rankinfo.rankings && rankinfo.rankings.length > 0) {
 		// 2020-02-08, M.O'C: Change 'currentrankings' into event-specific 'rankings'; enrich with event_key 
-		var thisRankings = rankinfo.rankings;
-		for (var thisRank of thisRankings) {
+		let thisRankings = rankinfo.rankings;
+		for (let thisRank of thisRankings) {
 			thisRank['event_key'] = eventKey;
 			rankArr.push(thisRank);
 		}
@@ -205,10 +210,10 @@ router.post('/updatematches', wrap(async (req, res) => {
 	await utilities.insert('rankings', rankArr);
 
 	// Get matches data from TBA
-	var url = 'event/' + eventKey + '/matches';
+	let url = 'event/' + eventKey + '/matches';
 	logger.debug('url=' + url);
-	var matchData = await utilities.requestTheBlueAlliance(url);
-	var arrayLength = matchData.length;
+	let matchData = await utilities.requestTheBlueAlliance(url);
+	let arrayLength = matchData.length;
 	if (arrayLength == null) {
 		logger.debug('Whoops, there was an error!');
 		logger.debug('data=' + matchData);
