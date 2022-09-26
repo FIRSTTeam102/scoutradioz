3c794fabc3701e4f219511b34fb7ba3a0cfe0bf9
Ardaglash <michael.p.oconnell@gmail.com>
2022-09-26 19:26:51 -0400

    Form editing now actually writes back to the DB

--- primary/src/routes/manage/orgconfig.ts
+++ primary/src/routes/manage/orgconfig.ts
@@ -5,6 +5,8 @@ import utilities from '@firstteam102/scoutradioz-utilities';
 import Permissions from '../../helpers/permissions';
 import e from '@firstteam102/http-errors';
 import type { Layout, LayoutEdit } from '@firstteam102/scoutradioz-types';
+import { DeleteResult, InsertManyResult } from 'mongodb';
+import { write } from 'fs';
 
 const router = express.Router();
 const logger = getLogger('orgconfig');
@@ -75,10 +77,11 @@ router.post('/submitform', wrap(async (req, res) => {
 	let thisUser = req._user;
 	// only let a user logged into the org modify their own org_key
 	let org_key = thisUser.org_key;
+	logger.debug('org_key=' + org_key);
 	
 	let jsonString = req.body.jsonData;
 	logger.debug('jsonString=' + jsonString);
-	let year = req.body.year;
+	let year = parseInt(req.body.year);
 	logger.debug('year=' + year);
 	let form_type = req.body.form_type;
 	logger.debug('form_type=' + form_type);
@@ -95,12 +98,19 @@ router.post('/submitform', wrap(async (req, res) => {
 	let updatedString = JSON.stringify(formdata);
 	logger.debug('updatedString=' + updatedString);
 
-	// TODO
-	// 1. delete existing data {if any} matching form_type, org_key, year
-	// 2. write in new/updated data
+	logger.warn('FOOOOOOOOOOOOOOOOOOOOOO');
 
-	//let writeResult = await utilities.insert('users', insertQuery);
-	
+	// 1. delete existing data {if any} matching form_type, org_key, year
+	let removeResult: DeleteResult = await utilities.remove('layout', {org_key: org_key, year: year, form_type: form_type});
+	logger.info(`Removed ${removeResult.deletedCount} prior form records`);
+
+	// 2. write in new/updated data
+	let writeResult: InsertManyResult<Document> | undefined = await utilities.insert('layout', formdata);
+	if (writeResult)
+		logger.info(`Inserted ${writeResult.insertedCount} new form records`);
+	else
+		logger.warn('Inserted 0 new form records!');
+
 	res.redirect('/manage');
 }));
 	
