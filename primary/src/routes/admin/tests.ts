036b3bad74af1d6ed5c424b067c322bfdf2c7eca
JL102 <jordanlees@mailbox.org>
2022-11-15 14:29:18 -0500

    Fixed typing errors (pre-merge with new-scouting-assignments branch)

--- primary/src/routes/admin/tests.ts
+++ primary/src/routes/admin/tests.ts
@@ -4,6 +4,7 @@ import wrap from '../../helpers/express-async-handler';
 import utilities from '@firstteam102/scoutradioz-utilities';
 import Permissions from '../../helpers/permissions';
 import type { User } from '@firstteam102/scoutradioz-types';
+import type { ObjectId } from 'mongodb';
 
 const router = express.Router();
 const logger = getLogger('tests');
@@ -33,10 +34,11 @@ router.all('/login-to-org-data', async (req, res, next) => {
 	const org_key = 'demo';
 	const org_passwd = 'demo2022'; // JL note: this isn't much of a secret, and if someone's gone far enough into SR's code to discover hidden passwords, then hello! Wanna join the dev team?
 	
-	let userWithoutPassword: User = await utilities.findOne('users', 
+	// JL note: Remove the "& {_id: ObjectId}" after merging new-scouting-assignments branch
+	let userWithoutPassword: User & {_id: ObjectId} = await utilities.findOne('users', 
 		{org_key: org_key, password: 'default'}
 	);
-	let userWithPassword: User = await utilities.findOne('users',
+	let userWithPassword: User & {_id: ObjectId} = await utilities.findOne('users',
 		{org_key: org_key, password: {$ne: 'default'}}
 	);
 	
@@ -76,7 +78,7 @@ router.all('/password-creation-data', async (req, res, next) => {
 		password: 'default'
 	};
 	await utilities.insert('users', tempUser);
-	let newUser: User = await utilities.findOne('users', {name: TEMP_USER_NAME});
+	let newUser: User & {_id: ObjectId} = await utilities.findOne('users', {name: TEMP_USER_NAME});
 	
 	res.send({
 		userForPasswordCreation: String(newUser._id),
