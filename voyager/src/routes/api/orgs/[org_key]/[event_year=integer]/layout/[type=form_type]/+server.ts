756788119782ecdc5f2bd60832bfc21beab46b28
JL102 <jordanlees@mailbox.org>
2023-03-07 17:48:18 -0500

    Added a bunch of APIs

--- voyager/src/routes/api/orgs/[org_key]/[event_year=integer]/layout/[type=form_type]/+server.ts
+++ voyager/src/routes/api/orgs/[org_key]/[event_year=integer]/layout/[type=form_type]/+server.ts
@@ -1,17 +1,14 @@
 import utilities from '$lib/server/utilities';
-import { error } from '@sveltejs/kit';
-import { org_key, event_year, getStore } from '$lib/stores';
+import { json } from '@sveltejs/kit';
 import type { LayoutField } from '$lib/types';
 import type { RequestHandler } from './$types';
 
 export const GET: RequestHandler = async ({ params }) => {
-	if (!['pit', 'match'].includes(params.type)) throw error(404, new Error('Invalid form type'));
-	
 	const form_type = (params.type + 'scouting') as 'pitscouting'|'matchscouting';
 
 	const layout: LayoutField[] = await utilities.find('layout', {
-		year: getStore(event_year),
-		org_key: getStore(org_key),
+		year: parseInt(params.event_year),
+		org_key: params.org_key,
 		form_type,
 		type: { $ne: 'derived' } // not needed for ui, might change later
 	}, {
@@ -19,9 +16,5 @@ export const GET: RequestHandler = async ({ params }) => {
 		projection: { _id: 0, form_type: 0, org_key: 0 }
 	}, { allowCache: true });
 
-	return new Response(JSON.stringify(layout), {
-		headers: {
-			'content-type': 'application/json'
-		}
-	});
+	return json(layout);
 };
--- voyager/src/routes/api/orgs/[org_key]/users/+server.ts
+++ voyager/src/routes/api/orgs/[org_key]/users/+server.ts
@@ -1,16 +1,12 @@
 import utilities from '$lib/server/utilities';
-// import type { User } from 'scoutradioz-types';
 import type { LightUser } from '$lib/localDB';
 import type { RequestHandler } from './$types';
-import { getStore, org_key } from '$lib/stores';
 import { json } from '@sveltejs/kit';
 
-export const GET: RequestHandler = async ({ url, params }) => {
+export const GET: RequestHandler = async ({ params }) => {
 
-	console.log(org_key, getStore(org_key));
-	
 	const users: LightUser[] = await utilities.find('users', {
-		org_key: getStore(org_key),
+		org_key: params.org_key
 	}, {
 		projection: {
 			_id: 1,
