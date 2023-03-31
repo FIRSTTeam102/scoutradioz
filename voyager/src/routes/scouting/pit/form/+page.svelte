b3943487dcafd4083b54227a82e8c6dbfe5f4073
JL102 <jordanlees@mailbox.org>
2023-03-30 21:27:57 -0400

    WIP Pit scouting existing data

--- voyager/src/routes/scouting/pit/form/+page.svelte
+++ voyager/src/routes/scouting/pit/form/+page.svelte
@@ -1,12 +1,28 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import ScoutingForm from '$lib/form/ScoutingForm.svelte';
+	import { event_key, org_key } from '$lib/stores';
+	import { onMount } from 'svelte';
+	import db from '$lib/localDB';
 
 	export let data: PageData;
 
 	let formData: {
 		[key: string]: unknown;
 	} = {};
+	
+	onMount(async () => {
+		// Retrieve pit scouting data from db if it exists, once, when the page loads
+		const existingData = await db.pitscouting.where({
+			org_key: $org_key,
+			event_key: $event_key,
+			team_key: data.key,
+		})
+		.first();
+		if (existingData && existingData.data) {
+			formData = existingData.data;
+		}
+	})
 </script>
 
 <ScoutingForm layout={data.layout} bind:formData teamNumber={data.teamNumber} />
--- voyager/src/routes/scouting/pit/form/+page.ts
+++ voyager/src/routes/scouting/pit/form/+page.ts
@@ -1,9 +1,13 @@
 import type { PageLoad } from './$types';
 import type { LayoutField } from '$lib/types';
+import { error } from '@sveltejs/kit';
 
 export const load: PageLoad = async ({ url, fetch }) => {
 	const key = url.searchParams.get('key');
+	
 	const teamNumber = Number(key?.replace('frc', ''));
+
+	if (!key || !teamNumber) throw error(404, new Error('Team key is either not defined or invalid'));
 
 	const layout: LayoutField[] = await (await fetch('/api/layout/pit')).json();
 
