22d1868068a5c2fe681012fed75553310a78d830
JL102 <jordanlees@mailbox.org>
2023-10-06 20:14:50 -0400

    Add vite-plugin-mkcert

--- voyager/vite.config.js
+++ voyager/vite.config.js
@@ -1,8 +1,15 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import mkcert from 'vite-plugin-mkcert';
 
 /** @type {import('vite').UserConfig} */
 const config = {
-	plugins: [sveltekit()],
+	plugins: [sveltekit(), mkcert()],
+	// JL note: It seems like after vite-plugin-mkcert downloads certificates, vite automatically
+	// 	serves https if server.https is not specified. I feel like it's useful to default to false
+	// 	unless you add --https to the command line args.
+	server: {
+		https: false,
+	},
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
 	},
