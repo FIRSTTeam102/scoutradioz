72673b9d8aaa1435d459a06abae85dcd3a9c8eb1
JL102 <jordanlees@mailbox.org>
2023-10-09 16:46:28 -0400

    Improved the match scouting form validation script

--- primary/public-src/ts/script-editform.ts
+++ primary/public-src/ts/script-editform.ts
@@ -1,12 +1,18 @@
-import type { LayoutEdit, StringDict } from 'scoutradioz-types';
+import type { AbsoluteValueOperation, CompareOperation, DivideOperation, LogOperation, MinMaxOperation, MultiplyOperation, MultiselectOperation, operand, SumOperation } from 'scoutradioz-types';
+
+type LayoutEdit = import('scoutradioz-types').LayoutEdit;
+type StringDict = import('scoutradioz-types').StringDict;
+type DerivedLayout = import('scoutradioz-types').DerivedLayout;
+
 // consolesdflkjd
-function validate() {
+async function validate() {
 	// get the JSON from the form
 	const jsonFieldElem = document.getElementById('jsonfield') as HTMLInputElement;
 	let jsonText = jsonFieldElem.value;
 
 	const previousKeysElem = document.getElementById('previousKeys') as HTMLInputElement;
 	let previousKeys = JSON.parse(previousKeysElem.value);
+	lightAssert(Array.isArray(previousKeys), 'previousKeys is not an array');
 	//console.log("previousKeys.length=" + previousKeys.length);
 
 	const formTypeElem = document.getElementById('form_type') as HTMLInputElement;
@@ -19,7 +25,12 @@ function validate() {
 		jsonData = JSON.parse(jsonText);
 	}
 	catch (error) {
-		NotificationCard.error('Invalid JSON! Please correct.');
+		if (error instanceof SyntaxError) {
+			NotificationCard.error(`Invalid JSON! Please correct. \n${error}`, { ttl: 0, exitable: true });
+		}
+		else {
+			NotificationCard.error('Invalid JSON! Please correct.');
+		}
 		return null;
 	}
 
@@ -67,32 +78,126 @@ function validate() {
 
 	// make sure required fields exist - otherNotes, contributedPoints [for matchScouting only]
 	if (formType == 'matchscouting') {
-		if (!idDict['otherNotes'])
-			NotificationCard.warn('WARNING! You have not defined \'otherNotes\'');
-		if (!idDict['contributedPoints'])
-			NotificationCard.warn('WARNING! You have not defined \'contributedPoints\'');
+		let otherNotes = idDict['otherNotes'];
+		let contributedPoints = idDict['contributedPoints'];
+		if (!otherNotes || !contributedPoints) {
+			let message = '*WARNING!* You have not defined the following required fields: ';
+			if (!otherNotes) message += '\n - otherNotes';
+			if (!contributedPoints) message += '\n - contributedPoints';
+			let { cancelled } = await Confirm.show(message, { yesText: 'OK' });
+			if (cancelled) return null;
+		}
 	}
 
-	// check if any existing data fields are not included
-	// TODO enable for Pit Scouting (will need changes in orgconfig.ts as well)
-	if (formType == 'matchscouting') {
-		for (let j = 0; j < previousKeys.length; j++) {
-			console.log(j + ' ' + previousKeys[j]);
-			if (!idDict[previousKeys[j]]) {
-				NotificationCard.warn('WARNING! JSON is missing existing data key ' + previousKeys[j]);
-				//return null;
+	// Validate derived metrics
+	const derivedLayouts = jsonData.filter(itm => itm.type === 'derived') as DerivedLayout[];
+	for (let derived of derivedLayouts) {
+		// For error messages
+		let derivedDescription = ` - for Derived Metric with id=${derived.id} and label=${derived.label}`;
+		lightAssert(Array.isArray(derived.operations), `Derived metric does not have an array of operations ${derivedDescription}`);
+		let finalOperation = derived.operations[derived.operations.length - 1];
+		lightAssert(!finalOperation.hasOwnProperty('as'), `The final derived operation must not have an 'as' keyword ${derivedDescription}`);
+		// Go through the list of operations one by one, validating the variables each step
+		let intermediateVariables: string[] = [];
+
+		const isValidOperand = (operand: operand) => {
+			// Ensure the variable reference is valid
+			if (typeof operand === 'string') {
+				if (operand.startsWith('$')) {
+					return intermediateVariables.includes(operand);
+				}
+				else {
+					return !!idDict[operand];
+				}
 			}
-		}
+			// just validate that the other operand is a number
+			else {
+				return typeof operand === 'number';
+			}
+		};
+
+		derived.operations.forEach((thisOp, i) => {
+			// JL: I don't like copy-pasted strings, so using a function for the different cases to validate whatever # of operands need to be validated
+			const validateOperands = (operandList: operand[]) => {
+				operandList.forEach(operand => {
+					lightAssert(isValidOperand(operand), `Operation #${i}, ${thisOp.operator}, has an invalid operand "${operand}" ${derivedDescription}`);
+				});
+			};
+
+			// Handle different operation types
+			switch (thisOp.operator) {
+				case 'multiselect': {
+					let op = thisOp as MultiselectOperation;
+					// Ensure the variable reference is valid
+					let thisMultiselectLayout = jsonData.find(item => item.id === op.id && item.type === 'multiselect');
+					lightAssert(thisMultiselectLayout, `Multiselect derived operation has an invalid id. Make sure it references either a variable earlier in the operand chain or the id of a non-derived metric. Invalid value=${op.id}, derived ${derivedDescription}`);
+					let thisMultiselectOpts = thisMultiselectLayout?.options;
+					lightAssert(Array.isArray(thisMultiselectOpts), `Multiselect with id ${thisMultiselectLayout.id} options is not an array`);
+					// Ensure the quantifiers are valid for the multiselect
+					for (let opt of thisMultiselectOpts) {
+						lightAssert(op.quantifiers.hasOwnProperty(opt), `Operation #${i}, multiselect, does not have a quantifier for the string ${opt} ${derivedDescription}`);
+					}
+					for (let varName in op.quantifiers) {
+						lightAssert(thisMultiselectOpts.includes(varName), `Operation #${i}, multiselect, has a quantifier for the string ${varName}, but that string is not an option for the multiselect ${derivedDescription}`);
+						// Also, check that the quantifier is a number
+						lightAssert(typeof op.quantifiers[varName] === 'number', `Operation #${i}, multiselect, has a non-number quantifier "${op.quantifiers[varName]}" for the string ${varName} ${derivedDescription}`);
+					}
+					break;
+				}
+				// Operations that take 2 operands
+				case 'subtract': case 'divide': case 'gt': case 'gte': case 'lt': case 'lte': case 'eq': case 'ne': case 'min': case 'max': {
+					let op = thisOp as | DivideOperation | CompareOperation | MinMaxOperation;
+					lightAssert(op.operands.length === 2, `Operation #${i}, ${op.operator}, needs exactly 2 operands ${derivedDescription}`);
+					validateOperands(op.operands);
+					break;
+				}
+				// Operations that take N operands
+				case 'add': case 'sum': case 'multiply': {
+					let op = thisOp as SumOperation | MultiplyOperation;
+					validateOperands(op.operands);
+					break;
+				}
+				// 1 operand
+				case 'abs': {
+					let op = thisOp as AbsoluteValueOperation;
+					lightAssert(op.operands.length === 1, `Operation #${i}, ${op.operator}, needs exactly 1 operand ${derivedDescription}`);
+					validateOperands(op.operands);
+					break;
+				}
+				// 1 operand then 1 number
+				case 'log': {
+					let op = thisOp as LogOperation;
+					lightAssert(op.operands.length === 2, `Operation #${i}, ${op.operator}, needs exactly 2 operands ${derivedDescription}`);
+					validateOperands([op.operands[0]]);
+					lightAssert(typeof op.operands[1] === 'number', `Operation #${i}, ${op.operator}, needs its second operand to be a number but found "${op.operands[1]}" ${derivedDescription}`);
+				}
+			}
+			// Add the output of this operation to the list of intermediate variables
+			if (i < derived.operations.length - 1) {
+				let thisOpAs = thisOp.as;
+				lightAssert(thisOpAs, `Operation #${i}, ${thisOp.operator} does not have an 'as' keyword ${derivedDescription}`);
+				intermediateVariables.push('$' + thisOpAs);
+			}
+		});
 	}
 
 	// verify keys are all 'allowed'
 	// TODO
 
+
+	// check if any existing data fields are not included
+	// TODO enable for Pit Scouting (will need changes in orgconfig.ts as well)
+	let missingDataKeys = previousKeys.filter((key) => !idDict[key]);
+	if (missingDataKeys.length > 0) {
+		let message = `*WARNING!* The following keys (ids) exist in your past match scouting data, but are not included in this layout: \n${missingDataKeys.map(key => `\n - ${key}`)}`;
+		let { cancelled } = await Confirm.show(message, { yesText: 'OK', noText: 'Cancel' });
+		if (cancelled) return null;
+	}
 	return jsonString;
 }
 
-function test() {
-	let jsonString = validate();
+async function test() {
+	let jsonString = await validate();
 	if (jsonString == null)
 		return;
 
@@ -101,11 +206,11 @@ function test() {
 	testDataElem.value = jsonString;
 	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 	// @ts-ignore
-	tdocument.forms.testForm.submit();
+	document.forms.testForm.submit();
 }
 
-function submit() {
-	let jsonString = validate();
+async function submit() {
+	let jsonString = await validate();
 	if (jsonString == null)
 		return;
 
@@ -116,3 +221,9 @@ function submit() {
 	// @ts-ignore
 	document.forms.submitForm.submit();
 }
+
+$(() => {
+	$('#testBtn').on('click', test);
+	$('#submitBtn').on('click', submit);
+	$('#validateBtn').on('click', validate);
+});
