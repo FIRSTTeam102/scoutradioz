1b29888b80317e5996e6dc6706123b61dec93d1b
JL102 <jordanlees@mailbox.org>
2023-10-18 19:53:42 -0400

    Create voyager-sst
    The plan is to add voyager files to voyager piece-by-piece,
    then rename voyager-sst to voyager. That way, we won't have any unnecessary
    old template files or whatever.

---
+++ voyager-sst/src/routes/+page.svelte
@@ -0,0 +1,3 @@
+<h1>Welcome to SvelteKit</h1>
+<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h2>Hello world this is supposed to be coming from AWS</h2>
