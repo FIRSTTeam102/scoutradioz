a0fbab42f70bb8647fb88a14a9cb6ab3914f625d
Ardaglash <michael.p.oconnell@gmail.com>
2023-11-04 20:50:27 -0400

    Update form generation to use local DB

--- voyager/src/routes/scouting/match/form/+page.ts
+++ voyager/src/routes/scouting/match/form/+page.ts
@@ -1,12 +1,21 @@
 import type { PageLoad } from './$types';
 import type { LayoutField } from '$lib/types';
+import { event_key, getStore, org_key } from '$lib/stores';
+import db from '$lib/localDB';
 
 export const load: PageLoad = async ({ url, fetch }) => {
 	const key = url.searchParams.get('key');
 	// todo: make more robust, probably into helper function, then consider making one route to show the form for both pit & match
 	const teamNumber = Number(key?.split('_')[2]?.replace('frc', ''));
 
-	const layout: LayoutField[] = await (await fetch('/api/layout/match')).json();
+	//const layout: LayoutField[] = await (await fetch('/api/layout/match')).json();
+	const layout = db.layout
+	.where({
+		org_key: getStore(org_key),
+		year: Number(getStore(event_key)?.substring(0,4)),
+		form_type: 'matchscouting'
+	})
+	.toArray();
 
 	return { layout, key, teamNumber };
 };
--- voyager/src/routes/scouting/pit/form/+page.ts
+++ voyager/src/routes/scouting/pit/form/+page.ts
@@ -1,6 +1,8 @@
 import type { PageLoad } from './$types';
 import type { LayoutField } from '$lib/types';
 import { error } from '@sveltejs/kit';
+import { event_key, getStore, org_key } from '$lib/stores';
+import db from '$lib/localDB';
 
 export const load: PageLoad = async ({ url, fetch }) => {
 	const key = url.searchParams.get('key');
@@ -9,7 +11,14 @@ export const load: PageLoad = async ({ url, fetch }) => {
 
 	if (!key || !teamNumber) throw error(404, new Error('Team key is either not defined or invalid'));
 
-	const layout: LayoutField[] = await (await fetch('/api/layout/pit')).json();
+	//const layout: LayoutField[] = await (await fetch('/api/layout/pit')).json();
+	const layout = db.layout
+	.where({
+		org_key: getStore(org_key),
+		year: Number(getStore(event_key)?.substring(0,4)),
+		form_type: 'pitscouting'
+	})
+	.toArray();
 
 	return { layout, key, teamNumber };
 };
