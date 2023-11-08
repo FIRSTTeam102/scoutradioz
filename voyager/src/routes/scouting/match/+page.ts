aed571009041c3a7cf08290c19a011685c47ec88
JL102 <jordanlees@mailbox.org>
2023-11-07 20:56:19 -0500

    Fixed match scouting alliances not being grouped by color
    Signed-off-by: JL102 <jordanlees@mailbox.org>

--- voyager/src/routes/scouting/match/+page.ts
+++ voyager/src/routes/scouting/match/+page.ts
@@ -1,5 +1,4 @@
 import type { PageLoad } from './$types';
-import type { MatchScouting } from 'scoutradioz-types';
 import { event_key, getStore, org_key, whenStoresLoaded } from '$lib/stores';
 import db from '$lib/localDB';
 
@@ -12,7 +11,7 @@ export const load: PageLoad = async ({ fetch }) => {
 			org_key: getStore(org_key),
 		})
 		.toArray();
-	
+
 	let grouped: (typeof all)[] = Object.values(
 		all.reduce((grouped: { [key: string]: any }, match) => {
 			grouped[match.match_key] = grouped[match.match_key] || [];
@@ -20,6 +19,12 @@ export const load: PageLoad = async ({ fetch }) => {
 			return grouped;
 		}, {})
 	).slice(0, 10);
+
+	// Make red alliance appear first 
+	// TODO: Maybe use the real red1, red2, etc. order from the matches table, but that would require sending that order in the qr code
+	grouped.forEach(group =>
+		group.sort((a, b) => b.alliance.localeCompare(a.alliance))
+	);
 
 	return { grouped };
 };
