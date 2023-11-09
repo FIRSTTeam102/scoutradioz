e0e2a281e7c287f7ce2e1857dc4ec5a86177b288
JL102 <jordanlees@mailbox.org>
2023-11-08 20:36:03 -0500

    Update Koder's version
    Signed-off-by: JL102 <jordanlees@mailbox.org>

--- voyager/static/lib/wasmQrWorker.js
+++ voyager/static/lib/wasmQrWorker.js
@@ -1,32 +1,30 @@
 // @ts-nocheck
 importScripts('/lib/zbar.js');
 importScripts('/lib/koder-browser.js');
-// import Koder from '/lib/koder-browser.js';
 
 (async () => {
-	// Initialize Koder
-	const koder = await new Koder().initialize({ wasmDirectory: '/lib' });
-	// const koder = await new Koder().initialized;
+  // Initialize Koder
+  const koder = await new Koder().initialize({wasmDirectory: "/lib"});
 
-	// Listen for messages from JS main thread containing raw image data
-	self.addEventListener('message', (event) => {
-		if ('width' in event.data && 'height' in event.data) {
-			self.width = event.data.width;
-			self.height = event.data.height;
-		}
+  // Listen for messages from JS main thread containing raw image data
+  self.addEventListener('message', event => {
+    if ('width' in event.data && 'height' in event.data) {
+      this.width = event.data.width;
+      this.height = event.data.height;
+    }
 
-		const { data } = event.data;
-		if (!data) return;
+    const {data} = event.data;
+    if (!data) return;
 
-		const t0 = new Date().getTime();
-		const scanResult = koder.decode(data, self.width, self.height);
-		const t1 = new Date().getTime();
-		if (scanResult) {
-			console.log(`Scanned in ${t1 - t0} ms`);
-			postMessage({
-				data: scanResult,
-				ms: t1 - t0
-			});
-		}
-	});
+    const t0 = new Date().getTime();
+    const scanResult = koder.decode(data, this.width, this.height);
+    const t1 = new Date().getTime();
+    if (scanResult) {
+      postMessage({
+        data: scanResult.code,
+        type: scanResult.type,
+        ms: t1-t0
+      });
+    }
+  });
 })();
