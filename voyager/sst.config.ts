bf6303ab30f712c776cd34a81db1ae1e27f60cf5
JL102 <jordanlees@mailbox.org>
2023-11-09 14:22:09 -0500

    eslint/sst tweaks

--- voyager/sst.config.ts
+++ voyager/sst.config.ts
@@ -1,32 +1,34 @@
-import type { SSTConfig } from "sst";
-import { SvelteKitSite } from "sst/constructs";
+import type { SSTConfig } from 'sst';
+import { SvelteKitSite } from 'sst/constructs';
+
+const allowedDeployStages = ['test', 'qa', 'prd'];
 
 export default {
-  config(_input) {
-    return {
-      name: "voyager-sst",
-      region: "us-east-1",
-    };
-  },
-  stacks(app) {
-    app.stack(function Site({ stack }) {
+	config(_input) {
+		return {
+			name: 'voyager-sst',
+			region: 'us-east-1',
+		};
+	},
+	stacks(app) {
+		app.stack(function Site({ stack }) {
 
-      let domainName =
-        stack.stage === 'prod' ? 'voyager.scoutradioz.com'
-          : `${stack.stage}.voyager.scoutradioz.com`;
+			let domainName =
+			stack.stage === 'prod' ? 'voyager.scoutradioz.com'
+				: `${stack.stage}.voyager.scoutradioz.com`;
 
-      const site = new SvelteKitSite(stack, "site", {
-        customDomain: {
-          domainName,
-          hostedZone: 'scoutradioz.com',
-        },
-        environment: {
-          TIER: stack.stage,
-        }
-      });
-      stack.addOutputs({
-        url: site.url,
-      });
-    });
-  },
+			const site = new SvelteKitSite(stack, 'site', {
+				customDomain: {
+					domainName,
+					hostedZone: 'scoutradioz.com',
+				},
+				environment: {
+					TIER: stack.stage,
+				}
+			});
+			stack.addOutputs({
+				url: site.url,
+			});
+		});
+	},
 } satisfies SSTConfig;
