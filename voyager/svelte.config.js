617f82e562dd4476e171b326268edf251208953b
JL102 <jordanlees@mailbox.org>
2024-01-20 13:39:07 -0500

    Update dependencies

--- voyager/svelte.config.js
+++ voyager/svelte.config.js
@@ -1,5 +1,5 @@
 import adapter from 'svelte-kit-sst';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +7,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		serviceWorker: {
-			register: true,
+			register: false,
 		}
 	},
 };
