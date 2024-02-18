db05c1a639d84df4fa7a5cc53595d1e3c22e4b5a
JL102 <jordanlees@mailbox.org>
2024-02-18 17:09:42 -0500

    Made service worker auto register when deploying, but not register when in dev mode

--- voyager/svelte.config.js
+++ voyager/svelte.config.js
@@ -1,5 +1,14 @@
 import adapter from 'svelte-kit-sst';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
+import { config as readDotEnv } from 'dotenv';
+
+readDotEnv();
+
+let registerServiceWorker = true;
+if (process.env.NODE_ENV === 'development' && process.env.REGISTER_SERVICE_WORKER !== 'true') {
+	registerServiceWorker = false;
+	console.warn('SvelteKit will NOT register the service worker because NodeJS is currently in development mode and process.env.REGISTER_SERVICE_WORKER is not set to "true".');
+}
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
@@ -7,7 +16,7 @@ const config = {
 	kit: {
 		adapter: adapter(),
 		serviceWorker: {
-			register: true,
+			register: registerServiceWorker
 		},
 	},
 	onwarn: (warning, handler) => {
