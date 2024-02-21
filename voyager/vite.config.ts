cd86311de6536bda9983e59c06bb1bf87e3489c0
JL102 <jordanlees@mailbox.org>
2024-02-21 15:53:15 -0500

    Add check for node-rs stuff when deploying

--- voyager/vite.config.ts
+++ voyager/vite.config.ts
@@ -1,5 +1,18 @@
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
+import fs from 'fs';
+import path from 'path';
+
+// If we're building the app, then we want to check and make sure we have the right node-rs modules in our node_modules
+if (process.env.NODE_ENV === 'production') {
+	let paths = [path.resolve('node_modules', '@node-rs', 'argon2-linux-arm64-gnu'), path.resolve('node_modules', '@node-rs', 'bcrypt-linux-arm64-gnu')];
+	for (let path of paths) {
+		if (!fs.existsSync(path)) {
+			console.error('Required binary packages for user login could not be found in node_modules! Try: yarn add --force --ignore-platform @node-rs/argon2 @node-rs/bcrypt');
+			process.exit(1);
+		}
+	}
+}
 
 export default defineConfig({
 	plugins: [sveltekit()],
