522974ed9409f1f8e62b325dc1fd08e9dbd0453d
JL102 <jordanlees@mailbox.org>
2024-02-24 10:44:55 -0500

    Disable some unused IBM Plex Sans fonts

--- voyager/src/service-worker.ts
+++ voyager/src/service-worker.ts
@@ -1,13 +1,23 @@
 // https://www.sarcevic.dev/offline-first-installable-pwa-sveltekit-workbox-precaching
 import { build, files, prerendered, version } from '$service-worker';
-import { precacheAndRoute } from 'workbox-precaching';
+import { precacheAndRoute, cleanupOutdatedCaches } from 'workbox-precaching';
+
+// Filter out any paths with ".unused" in the path, e.g. IBM Plex Sans fonts that are not being used
+let filteredFiles = files.filter(path => !path.match(/\.unused/g));
+
 const precache_list = [
 	'/', // Attention: might not be ideal for your use case - read more below.
 	...build,
-	...files,
+	...filteredFiles,
 	...prerendered
 ].map((s) => ({
 	url: s,
 	revision: version
 }));
-precacheAndRoute(precache_list);
+precacheAndRoute(precache_list, {
+	cleanURLs: false,
+});
+
+cleanupOutdatedCaches();
+
+// console.log('List of files that are being precached:', precache_list);
