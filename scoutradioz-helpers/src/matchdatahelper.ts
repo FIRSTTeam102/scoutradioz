4a3a550119946e4c88ce0cc62362fcd708a3d6cf
Ardaglash <michael.p.oconnell@gmail.com>
2025-02-10 23:59:15 +0000

    expanded matchdatahelper columns to 'isMetric'

--- scoutradioz-helpers/src/matchdatahelper.ts
+++ scoutradioz-helpers/src/matchdatahelper.ts
@@ -407,7 +407,7 @@ export class MatchDataHelper {
 			{ allowCache: true, maxCacheAge: 180 }
 		);
 		assert(schema);
-		const derivedLayout = schema.layout.filter(item => item.type === 'derived');
+		const derivedLayout = schema.layout.filter(item => MatchDataHelper.isMetric(item));
 		let t_dbEnd = performance.now();
 
 		const derivedCalculator = new DerivedCalculator(convertValuesDict(matchData));
@@ -490,7 +490,7 @@ export class MatchDataHelper {
 			{ allowCache: true, maxCacheAge: 180 }
 		);
 		assert(schema);
-		const scorelayoutDB = schema.layout.filter(item => item.type === 'derived');
+		const scorelayoutDB = schema.layout.filter(item => MatchDataHelper.isMetric(item));
 		//const scorelayoutDB = schema.layout.filter(item => item.id);
 		logger.trace(`scoreLayoutDB=${JSON.stringify(scorelayoutDB)}`);
 
@@ -543,9 +543,11 @@ export class MatchDataHelper {
 			for (let thisLayout of scorelayoutDB) {
 				//var thisLayout = scorelayoutDB[i];
 				if (this.isQuantifiableType(thisLayout.type)) {
-					assert(thisLayout.id, `Layout element has no ID: ${JSON.stringify(thisLayout)}`);
-					if (savedCols[thisLayout.id])
-						scorelayout.push(thisLayout);
+					if ('id' in thisLayout) {
+						assert(thisLayout.id, `Layout element has no ID: ${JSON.stringify(thisLayout)}`);
+						if (savedCols[thisLayout.id])
+							scorelayout.push(thisLayout);
+					}
 				}
 				else
 					scorelayout.push(thisLayout);
@@ -589,7 +591,7 @@ export class MatchDataHelper {
 			{ allowCache: true, maxCacheAge: 180 }
 		);
 		assert(schema);
-		const scorelayout = schema.layout.filter(item => item.type === 'derived');
+		const scorelayout = schema.layout.filter(item => MatchDataHelper.isMetric(item));
 		
 		logger.trace('scorelayout=' + JSON.stringify(scorelayout));
 
