7e11e9e5ec4f7ff2d447df4f02168e257f05cf1b
Maximilleon-Robespierre <maxwellagreaves@gmail.com>
2025-02-22 21:03:22 +0000

    more colors

--- primary/scripts/dbAddHeatMapColors.ts
+++ primary/scripts/dbAddHeatMapColors.ts
@@ -25,21 +25,26 @@ utilities.refreshTier();
     await utilities.insert('heatmapcolors', { name: 'Grayscale', key: 'grayscale', min: { r: 0, g: 0, b: 0 }, max: { r: 200, g: 200, b: 200 } })
     await utilities.insert('heatmapcolors', { name: 'Sunburst', key: 'sunburst', min: { r: 110, g: 8, b: 36 }, max: { r: 244, g: 179, b: 1 } })
     await utilities.insert('heatmapcolors', { name: 'City Night', key: 'citynight', min: { r: 41, g: 10, b: 73 }, max: { r: 203, g: 236, b: 40 } })
-    await utilities.insert('heatmapcolors', { name: '102!!!', key: '102', min: { r: 0, g: 0, b: 0 }, max: { r: 255, g: 144, b: 0 } })
+    await utilities.insert('heatmapcolors', { name: 'Inferno Twilight', key: 'infernotwilight', min: { r: 0, g: 0, b: 0 }, max: { r: 255, g: 144, b: 0 } })
+    await utilities.insert('heatmapcolors', { name: 'Beach', key: 'beach', min: { r: 2, g: 81, b: 150 }, max: { r: 253, g: 179, b: 56 } })
+    await utilities.insert('heatmapcolors', { name: 'Joker', key: 'joker', min: { r: 41, g: 94, b: 17 }, max: { r: 176, g: 18, b: 158 } })
+    await utilities.insert('heatmapcolors', { name: 'Orple', key: 'orple', min: { r: 81, g: 40, b: 136 }, max: { r: 235, g: 97, b: 35 } })
+    await utilities.insert('heatmapcolors', { name: 'Cotton Candy', key: 'cottoncandy', min: { r: 126, g: 41, b: 84 }, max: { r: 148, g: 203, b: 236 } })
+    await utilities.insert('heatmapcolors', { name: 'Test1', key: 'test1', min: { r: 106, g: 74, b: 60 }, max: { r: 15, g: 101, b: 161 } })
+    await utilities.insert('heatmapcolors', { name: 'test2', key: 'test2', min: { r: 255, g: 190, b: 106 }, max: { r: 64, g: 176, b: 166 } })
+    await utilities.insert('heatmapcolors', { name: 'test3', key: 'test3', min: { r: 73, g: 41, b: 10 }, max: { r: 40, g: 203, b: 236 } })
 
     process.exit(0);
 })();
-
-
 // grayscale min(0,0,0), max(200,200,200)!
 // default min(63,0,0),max(0,214,0)!
-// blue to yellow min(2,81,150), max(253,179,56)
-// purple to orange min(81,40,136), max(235,97,35)
-// green to purple min(41,94,17), max(176,18,158)
+// blue to yellow min(2,81,150), max(253,179,56)!
+// purple to orange min(81,40,136), max(235,97,35)!
+// green to purple min(41,94,17), max(176,18,158)!
 // brown to blue min(106,74,60), max(15,101,161)
 // brick to yellow min(110,8,36), max(244,179,1)!
 // tan to turquoise min(255,190,106), max(64,176,166)
-// purple to sky min(126,41,84), max(148,203,236)
+// purple to sky min(126,41,84), max(148,203,236)!
 //brown to teal min(73,41,10), max(40,203,236)
 // purple to yellow min(41,10,73), max(203,236,40)!
-//102 min(0,0,0), max(255,144,0)
+//102 min(0,0,0), max(255,144,0)!
