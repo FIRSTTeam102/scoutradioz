8d580b3ceb506bf418e1b98eb3a1c44b99cd8c59
Daniel David <114867882+pigrammer3@users.noreply.github.com>
2025-08-06 15:01:06 -0400

    update voyager & primary to use new orgschema format

--- voyager/src/routes/api/orgs/[org_key]/[event_year=integer]/layout/[type=form_type]/+server.ts
+++ voyager/src/routes/api/orgs/[org_key]/[event_year=integer]/layout/[type=form_type]/+server.ts
@@ -9,12 +9,16 @@ export const GET: RequestHandler = async ({ params, locals }) => {
 	validateUserOrg(locals, params.org_key);
 	const form_type = (params.type + 'scouting') as 'pitscouting'|'matchscouting';
 	
+	// Get the current event for this org to find the event_key
+	const org = await utilities.findOne('orgs', { org_key: params.org_key });
+	httpAssert(org?.event_key, 404, `No current event set for org ${params.org_key}`);
+	
 	const orgschema = await utilities.findOne('orgschemas',
-		{org_key: params.org_key, form_type, year: parseInt(params.event_year)},
+		{org_key: params.org_key, form_type, event_key: org.event_key},
 		{},
 		{ allowCache: true },
 	);
-	httpAssert(orgschema, 500, `Org schema not found, ${params.org_key}, ${params.event_year}`);
+	httpAssert(orgschema, 500, `Org schema not found, ${params.org_key}, ${org.event_key}`);
 	const schema = await utilities.findOne('schemas',
 		{_id: orgschema.schema_id},
 		{},
