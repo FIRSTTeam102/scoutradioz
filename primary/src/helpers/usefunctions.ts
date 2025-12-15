91aecbf6be692661777d98794727536de69e635d
Ardaglash <michael.p.oconnell@gmail.com>
2025-12-15 23:43:08 +0000

    Bugfix of the cookie trim bugfix

--- primary/src/helpers/usefunctions.ts
+++ primary/src/helpers/usefunctions.ts
@@ -27,7 +27,7 @@ class UseFunctions {
 		}
 		
 		// If the user has not viewed this news item yet, AND it hasn't yet expired, send the summary to the client for it to display
-		if (req.cookies['last_news_update_read'].trim() !== platformSettings.news.cookie_value.trim() && new Date() < platformSettings.news.expires) {
+		if ((req.cookies['last_news_update_read'] ? req.cookies['last_news_update_read'].trim() : undefined) !== (platformSettings.news.cookie_value ? platformSettings.news.cookie_value.trim() : undefined) && new Date() < platformSettings.news.expires) {
 			res.cookie('news_update_to_display', platformSettings.news.summary); // string for client to display
 			res.cookie('news_update_id', platformSettings.news.cookie_value.trim()); // cookie value for client to set to last_news_update_read after they dismiss it
 		}
